0e24dce25cf2e8cf52b17757b0fbdc7426d43414
Souhail SERGHINE <Souhail.SERGHINE@esprit.tn>
2026-02-16 09:32:51 +0100

    base de donnes

--- main.cpp
+++ main.cpp
@@ -27,6 +27,22 @@ int main(int argc, char *argv[])
     // ────────────────────────────────────────────────────────────
 
     MainWindow w;
-    w.show();
+    Connection c;
+    bool test = c.createconnect();
+    if (test) {
+        w.show();
+        QMessageBox::information(nullptr, QObject::tr("database is open"),
+                                 QObject::tr("connection successful.\n"
+                                             "Click Cancel to exit."), QMessageBox::Cancel);
+    } else {
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                              QObject::tr("connection failed.\n"
+                                          "Click Cancel to exit."), QMessageBox::Cancel);
+    }
     return a.exec();
 }
+
+
+
+
+
