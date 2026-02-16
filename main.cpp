d354563fc0acde9fe96652dd7f9b32698fa92ad9
Souhail SERGHINE <Souhail.SERGHINE@esprit.tn>
2026-02-16 09:32:51 +0100

    base de donnes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "mainwindow.h"
-
+#include <QMessageBox>
 #include <QApplication>
 #include <QFile>
+#include "connection.h"
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +17,22 @@ int main(int argc, char *argv[])
     }
     
     MainWindow w;
-    w.show();
+    Connection c;
+    bool test = c.createconnect();
+    if (test) {
+        w.show();
+        QMessageBox::information(nullptr, QObject::tr("database is open"),
+                                 QObject::tr("connection successful.\n"
+                                             "Click Cancel to exit."), QMessageBox::Cancel);
+    } else {
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                              QObject::tr("connection failed.\n"
+                                          "Click Cancel to exit."), QMessageBox::Cancel);
+    }
     return a.exec();
 }
+
+
+
+
+
